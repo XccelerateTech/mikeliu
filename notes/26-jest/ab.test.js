a35267f7aeff4c8ec63efcd0517cd9b956f4067c
Mike Liu <mliu8m@gmail.com>
2019-04-09 09:59:53 +0800

    updated

---
+++ notes/26-jest/ab.test.js
@@ -0,0 +1,24 @@
+// jest exercise a&b
+
+describe('testing the test suite Jest', () => {
+    test('I am in the test block 1', () => {
+        console.log(`I am in the test block 1`);
+    });
+
+    test('I am in the test block 2', () => {
+        console.log(`I am in the test block 2`);
+    });
+
+    test('I am in the test block 3', () => {
+        console.log(`I am in the test block 3`);
+    });
+
+    test('I am in the test block 4', () => {
+        console.log(`I am in the test block 4`);
+    });
+
+    test('I am in the test block 5 but I fail', () => {
+        console.log('I am no. 5 but I fail');
+        throw new Error();
+    });
+});
